47953ba6ab7ed441a95469e9bc9483eb5f055ddb
Xac Williams <whitney.zac@gmail.com>
2025-04-05 21:07:03 +0000

    updated insert_data.sh to create necessary databases

--- insert_data.sh
+++ insert_data.sh
@@ -8,3 +8,16 @@ else
 fi
 
 # Do not change code above this line. Use the PSQL variable above to query your database.
+$($PSQL "CREATE DATABASE gamesDB")
+
+$($PSQL "CREATE TABLE games (id INTEGER NOT NULL,
+    firstname VARCHAR(50),
+    lastname VARCHAR(50),
+    email VARCHAR(50),
+    PRIMARY KEY (id))")
+
+$($PSQL "LOAD DATA INFILE 'games.csv'
+INTO TABLE games
+FIELDS TERMINATED BY ','
+IGNORE 1 ROWS")
+
